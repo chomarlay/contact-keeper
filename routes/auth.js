45907ec41cd0078b463877deeb8aaa334c58da60
chomarlay <chomarlay.aung@gmail.com>
2020-02-10 15:38:49 +1100

    Section 7 - Auth middleware and protecting Routes

--- routes/auth.js
+++ routes/auth.js
@@ -6,12 +6,19 @@ const jwt = require('jsonwebtoken');
 const config = require('config');
 const jwtsecret = config.get('jwtsecret');
 const { check, validationResult } = require('express-validator/check');
+const auth = require('../middleware/auth');
 
 // @route   GET api/auth
 // @desc    Get logged in user
 // @access  Private
-router.get('/', (req, res) => {
-  res.send('Get logged in user');
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ msg: 'Server Error' });
+  }
 });
 
 // @route   POST api/auth
