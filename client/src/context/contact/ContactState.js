f7d79097195da545cdd6140387ac918be4fe52f1
chomarlay <chomarlay.aung@gmail.com>
2020-02-14 23:07:30 +1100

    Section 8 - delete contact form UI

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -56,7 +56,9 @@ const ContactState = props => {
   };
 
   // Delete Contact
-
+  const deleteContact = id => {
+    dispatch({ type: 'DELETE_CONTACT', payload: id });
+  };
   // Update Contact
 
   // Set Current
@@ -71,7 +73,8 @@ const ContactState = props => {
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
-        addContact
+        addContact,
+        deleteContact
       }}
     >
       {props.children}
