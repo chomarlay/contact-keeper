e72c9ea4b0da9055e06decc0848c6e5e70498028
chomarlay <chomarlay.aung@gmail.com>
2020-02-24 22:51:05 +1100

    Section - Auth Context and Initial State

---
+++ client/src/context/auth/AuthState.js
@@ -0,0 +1,51 @@
+import React, { useReducer } from 'react';
+import AuthReducer from './authReducer';
+import AuthContext from './authContext';
+
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT,
+  CLEAR_ERRORS
+} from '../types';
+
+const AuthState = props => {
+  const initialState = {
+    user: null,
+    token: null,
+    isAuthenticated: null,
+    loading: true,
+    error: null
+  };
+
+  const [state, dispatch] = useReducer(AuthReducer, initialState);
+  // Load User
+
+  // Register User
+
+  // Login User
+
+  // Logout
+
+  // Clear Errors
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user: state.user,
+        token: state.token,
+        isAuthenticated: state.isAuthenticated,
+        loading: state.loading,
+        error: state.error
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default ContactState;
