3531baea4c2d4c97f0f46b34ad24212022f1db45
chomarlay <chomarlay.aung@gmail.com>
2020-03-12 05:27:42 +1100

    Prepare for deploy to Heroku

--- server.js
+++ server.js
@@ -19,8 +19,10 @@ app.use('/api/contacts', require('./routes/contacts'));
 // Serve static assets in production -- to deploy in Heroku
 //NOTE:  this has to be after the above Define Routes
 if (process.env.NODE_ENV === 'production') {
-  // Set static folder -- serve client 
-  app.use('*', (req, res) ==> res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+  // Set static folder -- serve client
+  app.use('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
 }
 
 const PORT = process.env.PORT || 5000;
