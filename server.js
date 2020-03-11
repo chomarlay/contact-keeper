b2871c75acc755606e69d956b01d5af0de10b91e
chomarlay <chomarlay.aung@gmail.com>
2020-03-12 05:42:48 +1100

    corrected server.js deployment to heroku take 2

--- server.js
+++ server.js
@@ -20,6 +20,8 @@ app.use('/api/contacts', require('./routes/contacts'));
 //NOTE:  this has to be after the above Define Routes
 if (process.env.NODE_ENV === 'production') {
   // Set static folder -- serve client
+  app.use(express.static('client/build'));
+
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   );
